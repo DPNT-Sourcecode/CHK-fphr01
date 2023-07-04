9d26880ad167446e24d114e813aed5264f52834f
sbx_user1051 <sbx_user1051@169.254.207.141>
2023-07-04 11:02:41 +0000

    Tue Jul 04 11:02:41 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -15,10 +15,11 @@ OFFERS = {
     }
 
 def checkout(skus:str) -> int:
-    skus_present = set(skus) 
     if not isinstance(skus, str) or not set(skus).issubset(PRICES):
         return -1
     
+    skus_present = set(skus) 
+
     value = 0
     
     for item in skus_present:
@@ -28,7 +29,12 @@ def checkout(skus:str) -> int:
             offer_quantity, single_quantity = divmod(quantity_of_item, required_quantity)
             
             value += offer_quantity*OFFERS[item][1]
-            value += offer_quantity*PRICES[item][1]
+            value += single_quantity*PRICES[item]
+            
+        else:
+            value += quantity_of_item*PRICES[item]
+            
+    return value
             
 
 
