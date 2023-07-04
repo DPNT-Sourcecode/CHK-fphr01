57962b767c990fd5a5a8e46e5d36c4a7f0d28d07
sbx_user1051 <sbx_user1051@169.254.207.141>
2023-07-04 13:48:49 +0000

    Tue Jul 04 13:48:49 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,34 +1,33 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 PRICES = {
-    "A": "50",
-    "B": "30",
-    "C": "20",
-    "D": "15",
-    "E": "40",
-    "F": "10",
-    "G": "20",
-    "H": "10",
-    "I": "35",
-    "J": "60",
-    "K": "70",
-    "L": "90",
-    "M": "15",
-    "N": "40",
-    "O": "10",
-    "P": "50",
-    "Q": "30",
-    "R": "50",
-    "S": "20",
-    "T": "20",
-    "U": "40",
-    "V": "50",
-    "W": "20",
-    "X": "17",
-    "Y": "20",
-    "Z": "21",
+    "A": 50,
+    "B": 30,
+    "C": 20,
+    "D": 15,
+    "E": 40,
+    "F": 10,
+    "G": 20,
+    "H": 10,
+    "I": 35,
+    "J": 60,
+    "K": 70,
+    "L": 90,
+    "M": 15,
+    "N": 40,
+    "O": 10,
+    "P": 50,
+    "Q": 30,
+    "R": 50,
+    "S": 20,
+    "T": 20,
+    "U": 40,
+    "V": 50,
+    "W": 20,
+    "X": 17,
+    "Y": 20,
+    "Z": 21,
 }
-# tuple with quantity required and offer price
 OFFERS = {
     "A": {3: 130, 5: 200},
     "B": {2: 45},
@@ -71,31 +70,31 @@ def checkout(skus: str) -> int:
 
     skus_present_count = delete_empty_counts(skus_present_count)
 
-    for group in GROUP_OFFERS.keys():
-        quantity_required = GROUP_OFFERS[group][0]
-        offer_price = GROUP_OFFERS[group][1]
+    # for group in GROUP_OFFERS.keys():
+    #     quantity_required = GROUP_OFFERS[group][0]
+    #     offer_price = GROUP_OFFERS[group][1]
 
-        valid_for_group_offer_count = []
-        for sku in group:
-            valid_for_group_offer_count.append(
-                [sku, skus_present_count.get(sku, 0), PRICES[sku]]
-            )
-        # sort list to prioritise higher value items
-        sorted_list = sorted(
-            valid_for_group_offer_count, key=lambda x: x[2], reverse=True
-        )
+    #     valid_for_group_offer_count = []
+    #     for sku in group:
+    #         valid_for_group_offer_count.append(
+    #             [sku, skus_present_count.get(sku, 0), PRICES[sku]]
+    #         )
+    #     # sort list to prioritise higher value items
+    #     sorted_list = sorted(
+    #         valid_for_group_offer_count, key=lambda x: x[2], reverse=True
+    #     )
 
-        group_items = ""
-        for sku_list in sorted_list:
-            while len(group_items) <= quantity_required or sku_list[1] > 0:
-                if len(group_items) == quantity_required:
-                    value += offer_price
-                    for item in group_items:
-                        skus_present_count[item] = max(
-                            0, skus_present_count.get(item, 0) - 1
-                        )
-                group_items += sku_list[0]
-                sku_list[1] -= 1
+    #     group_items = ""
+    #     for sku_list in sorted_list:
+    #         while len(group_items) <= quantity_required or sku_list[1] > 0:
+    #             if len(group_items) == quantity_required:
+    #                 value += offer_price
+    #                 for item in group_items:
+    #                     skus_present_count[item] = max(
+    #                         0, skus_present_count.get(item, 0) - 1
+    #                     )
+    #             group_items += sku_list[0]
+    #             sku_list[1] -= 1
 
     for item in OFFERS.keys():
         if item in skus_present_count:
@@ -120,4 +119,5 @@ def delete_empty_counts(count_dict: dict) -> dict:
         count_dict.pop(key)
 
     return count_dict
+
 
