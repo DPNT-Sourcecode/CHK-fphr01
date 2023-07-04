9beb9feb457d42c243a2aee8659ba90f18679144
sbx_user1051 <sbx_user1051@169.254.207.141>
2023-07-04 13:18:49 +0000

    Tue Jul 04 13:18:49 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,32 +1,32 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 PRICES = {
-    "A": 50,  
-    "B": 30, 
-    "C": 20, 
-    "D": 15, 
-    "E": 40, 
-    "F": 10, 
-    "G": 20, 
-    "H": 10, 
-    "I": 35, 
-    "J": 60, 
-    "K": 80, 
-    "L": 90, 
-    "M": 15, 
-    "N": 40, 
-    "O": 10, 
-    "P": 50, 
-    "Q": 30, 
-    "R": 50, 
-    "S": 30, 
-    "T": 20, 
-    "U": 40, 
-    "V": 50, 
-    "W": 20, 
-    "X": 90, 
-    "Y": 10, 
-    "Z": 50,
+    "A": "50",
+    "B": "30",
+    "C": "20",
+    "D": "15",
+    "E": "40",
+    "F": "10",
+    "G": "20",
+    "H": "10",
+    "I": "35",
+    "J": "60",
+    "K": "70",
+    "L": "90",
+    "M": "15",
+    "N": "40",
+    "O": "10",
+    "P": "50",
+    "Q": "30",
+    "R": "50",
+    "S": "20",
+    "T": "20",
+    "U": "40",
+    "V": "50",
+    "W": "20",
+    "X": "17",
+    "Y": "20",
+    "Z": "21",
 }
 # tuple with quantity required and offer price
 OFFERS = {
@@ -34,7 +34,7 @@ OFFERS = {
     "B": {2: 45},
     "F": {3: 20},
     "H": {5:45, 10:80},
-    "K": {2:150},
+    "K": {2:120},
     "P": {5: 200},
     "Q": {3:80},
     "U": {4: 120},
@@ -76,6 +76,8 @@ def checkout(skus:str) -> int:
         for sku in group:
             valid_for_group_offer_count.append([sku,skus_present_count.get(sku, 0), PRICES[sku]])
         sorted_list = sorted(valid_for_group_offer_count, key=lambda x: x[2])
+        
+        for 
 
         
                     
@@ -103,4 +105,5 @@ def delete_empty_counts(count_dict: dict) -> dict:
         count_dict.pop(key)
         
     return count_dict
+
 
